9e9bc841877c034045ebd7bdc12982cab89c5fbc
YdrMaster <ydrml@hotmail.com>
2024-12-26 17:49:07 +0800

    init
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+﻿# mem-rearrange
+
+[![CI](https://github.com/InfiniTensor/mem-rearrange/actions/workflows/build.yml/badge.svg?branch=main)](https://github.com/InfiniTensor/mem-rearrange/actions)
+[![Latest version](https://img.shields.io/crates/v/mem-rearrange.svg)](https://crates.io/crates/mem-rearrange)
+[![Documentation](https://docs.rs/mem-rearrange/badge.svg)](https://docs.rs/mem-rearrange)
+[![license](https://img.shields.io/github/license/InfiniTensor/mem-rearrange)](https://mit-license.org/)
+
+![GitHub repo size](https://img.shields.io/github/repo-size/InfiniTensor/mem-rearrange)
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/InfiniTensor/mem-rearrange)
+[![GitHub Issues](https://img.shields.io/github/issues/InfiniTensor/mem-rearrange)](https://github.com/InfiniTensor/mem-rearrange/issues)
+[![GitHub Pull Requests](https://img.shields.io/github/issues-pr/InfiniTensor/mem-rearrange)](https://github.com/InfiniTensor/mem-rearrange/pulls)
+![GitHub contributors](https://img.shields.io/github/contributors/InfiniTensor/mem-rearrange)
+![GitHub commit activity](https://img.shields.io/github/commit-activity/m/InfiniTensor/mem-rearrange)
---
+++ src/lib.rs
@@ -0,0 +1,217 @@
+#![doc = include_str!("../README.md")]
+#![deny(warnings)]
+
+use itertools::izip;
+use ndarray_layout::ArrayLayout;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use std::{cmp::Ordering, ptr::copy_nonoverlapping};
+
+pub extern crate ndarray_layout;
+
+// Layout: | unit | dst offset | src offset | count | idx strides | dst strides | src strides |
+#[derive(Clone, Debug)]
+#[repr(transparent)]
+pub struct Rearranging(Box<[isize]>);
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+#[repr(u8)]
+pub enum SchemeError {
+    ShapeMismatch,
+    DimReduce,
+}
+
+impl Rearranging {
+    pub fn new<const M: usize, const N: usize>(
+        dst: &ArrayLayout<M>,
+        src: &ArrayLayout<N>,
+        unit: usize,
+    ) -> Result<Self, SchemeError> {
+        // # 检查基本属性
+        let ndim = dst.ndim();
+        if src.ndim() != ndim {
+            return Err(SchemeError::ShapeMismatch);
+        }
+        // # 输入形状
+        #[derive(Clone, PartialEq, Eq, Debug)]
+        struct Dim {
+            len: usize,
+            dst: isize,
+            src: isize,
+        }
+        let mut dims = Vec::with_capacity(ndim);
+        for (&dd, &ds, &sd, &ss) in izip!(dst.shape(), src.shape(), dst.strides(), src.strides()) {
+            if dd != ds {
+                return Err(SchemeError::ShapeMismatch);
+            }
+            // 剔除初始的 1 长维度
+            if dd != 1 {
+                if sd == 0 {
+                    return Err(SchemeError::DimReduce);
+                }
+                dims.push(Dim {
+                    len: dd,
+                    dst: sd,
+                    src: ss,
+                })
+            }
+        }
+        // # 排序
+        dims.sort_unstable_by(|a, b| {
+            use Ordering::Equal as Eq;
+            match a.dst.abs().cmp(&b.dst.abs()) {
+                Eq => match a.src.abs().cmp(&b.src.abs()) {
+                    Eq => a.len.cmp(&b.len),
+                    ord => ord.reverse(),
+                },
+                ord => ord.reverse(),
+            }
+        });
+        // # 合并连续维度
+        let mut unit = unit as isize;
+        let mut ndim = dims.len();
+        // ## 合并末尾连续维度到 unit
+        for dim in dims.iter_mut().rev() {
+            if dim.dst == unit && dim.src == unit {
+                unit *= dim.len as isize;
+                ndim -= 1
+            } else {
+                break;
+            }
+        }
+        dims.truncate(ndim);
+        // ## 合并任意连续维度
+        for i in (1..dims.len()).rev() {
+            let (head, tail) = dims.split_at_mut(i);
+            let f = &mut head[i - 1]; // f for front
+            let b = &mut tail[0]; // b for back
+            let len = b.len as isize;
+            if b.dst * len == f.dst && b.src * len == f.src {
+                *f = Dim {
+                    len: b.len * f.len,
+                    dst: b.dst,
+                    src: b.src,
+                };
+                *b = Dim {
+                    len: 1,
+                    dst: 0,
+                    src: 0,
+                };
+                ndim -= 1
+            }
+        }
+        // # 合并空间
+        let mut ans = Self(vec![0isize; 4 + ndim * 3].into_boxed_slice());
+        ans.0[0] = unit as _;
+        ans.0[1] = dst.offset();
+        ans.0[2] = src.offset();
+        let layout = &mut ans.0[3..];
+        layout[ndim] = 1;
+        for (i, Dim { len, dst, src }) in dims.into_iter().filter(|d| d.len != 1).enumerate() {
+            layout[i] = len as _;
+            layout[1 + ndim + i] = dst;
+            layout[1 + ndim * 2 + i] = src;
+        }
+        for i in (1..=ndim).rev() {
+            layout[i - 1] *= layout[i];
+        }
+        Ok(ans)
+    }
+
+    #[inline]
+    pub fn ndim(&self) -> usize {
+        (self.0.len() - 4) / 3
+    }
+
+    #[inline]
+    pub fn unit(&self) -> usize {
+        self.0[0] as _
+    }
+
+    #[inline]
+    pub fn dst_offset(&self) -> isize {
+        self.0[1]
+    }
+
+    #[inline]
+    pub fn src_offset(&self) -> isize {
+        self.0[2]
+    }
+
+    #[inline]
+    pub fn count(&self) -> usize {
+        self.0[3] as _
+    }
+
+    #[inline]
+    pub fn idx_strides(&self) -> &[isize] {
+        let ndim = self.ndim();
+        &self.0[4..][..ndim]
+    }
+
+    #[inline]
+    pub fn dst_strides(&self) -> &[isize] {
+        let ndim = self.ndim();
+        &self.0[4 + ndim..][..ndim]
+    }
+
+    #[inline]
+    pub fn src_strides(&self) -> &[isize] {
+        let ndim = self.ndim();
+        &self.0[4 + ndim * 2..][..ndim]
+    }
+
+    pub fn shape(&self) -> impl Iterator<Item = usize> + '_ {
+        let ndim = self.ndim();
+        self.0[3..][..ndim + 1]
+            .windows(2)
+            .map(|pair| (pair[0] / pair[1]) as usize)
+    }
+
+    /// # Safety
+    ///
+    /// `dst` and `src` must be valid pointers and must able to access with the scheme.
+    pub unsafe fn launch(&self, dst: *mut u8, src: *const u8) {
+        let dst = dst.byte_offset(self.dst_offset());
+        let src = src.byte_offset(self.src_offset());
+        match self.count() {
+            1 => copy_nonoverlapping(src, dst, self.unit()),
+            count => {
+                let dst = dst as isize;
+                let src = src as isize;
+                let idx_strides = self.idx_strides();
+                let dst_strides = self.dst_strides();
+                let src_strides = self.src_strides();
+                (0..count as isize).into_par_iter().for_each(|mut rem| {
+                    let mut dst = dst;
+                    let mut src = src;
+                    for (i, &s) in idx_strides.iter().enumerate() {
+                        let k = rem / s;
+                        dst += k * dst_strides[i];
+                        src += k * src_strides[i];
+                        rem %= s
+                    }
+                    copy_nonoverlapping::<u8>(src as _, dst as _, self.unit())
+                })
+            }
+        }
+    }
+}
+
+#[test]
+fn test_scheme() {
+    let shape = [4, 3, 2, 1, 2, 3, 4];
+    let dst = [288, 96, 48, 48, 24, 8, 2];
+    let src = [576, 192, 96, 48, 8, 16, 2];
+    let dst = ArrayLayout::<7>::new(&shape, &dst, 0);
+    let src = ArrayLayout::<7>::new(&shape, &src, 0);
+    let scheme = Rearranging::new(&dst, &src, 2).unwrap();
+    assert_eq!(scheme.ndim(), 3);
+    assert_eq!(scheme.dst_offset(), 0);
+    assert_eq!(scheme.src_offset(), 0);
+    assert_eq!(scheme.unit(), 8);
+    assert_eq!(scheme.count(), 24 * 2 * 3);
+    assert_eq!(scheme.idx_strides(), [6, 3, 1]);
+    assert_eq!(scheme.dst_strides(), [48, 24, 8]);
+    assert_eq!(scheme.src_strides(), [96, 8, 16]);
+    assert_eq!(scheme.shape().collect::<Vec<_>>(), [24, 2, 3]);
+}
